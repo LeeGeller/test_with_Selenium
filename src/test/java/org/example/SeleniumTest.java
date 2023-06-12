3be4d6d1f2c06694d223d028c503e5c3ac247288
Kristina Arkadievna <108188624+LeeGeller@users.noreply.github.com>
2023-06-12 10:59:09 +0300

    first commit

---
+++ src/test/java/org/example/SeleniumTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeleniumTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+
+            driver = new FirefoxDriver();
+        }
+
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    void shouldWorkWithValidPath(){
+        driver.get("http://localhost:9999");
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Фамилия Имя");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79998887700");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.tagName("button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+
+        assertEquals(expected, actual);
+
+
+    }
+
+
+}
